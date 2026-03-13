226663d731a2e9dcffa1ffab7b855dfbbc5e476a
anisio-ifsp <anisio.silva@ifsp.edu.br>
2026-03-13 19:27:00 -0300

    aula 07

--- Aula_07_funcao_Part_II/src/callback.ts
+++ Aula_07_funcao_Part_II/src/callback.ts
@@ -1,9 +1,16 @@
-function concatenar(a: string, b: string): void {
-    var op = a + " " + b;
-    console.log("A operacao efetuada resultou em: " + op);
+function imprimir(param: any):void{
+    console.log("A operacao efetuada resultou em: " + param);
 }
 
-function somar(a: number, b: number): void {
-    var op = a + b;
-    console.log("A operacao efetuada resultou em: " + op);
+function concatenar(a: string, b: string, callback:(param:string)=>void): void {
+    var op = a + " " + b;
+    callback(op);
 }
+
+function somar(a: number, b: number, callback:(param:number)=> void): void {
+    var op = a + b;
+    callback(op);
+}
+
+concatenar("Hello", "World", imprimir );
+somar(2,4, imprimir)
---
+++ Aula_07_funcao_Part_II/src/sort.ts
@@ -0,0 +1,12 @@
+const numerosX: number[] = [33 ,22 ,1 ,2, 5, 4, 3];
+numerosX.sort();
+numerosX.sort((a,b)=> b - a);
+console.log(numerosX)
+
+const nomes: string[] = [ 'Xavier', 'Anisio', 'Rafael', 'Pedro'];
+
+nomes.sort((a,b) => a.localeCompare(b));
+
+console.log(nomes);
+
+
