f8917646f5574c402bff16e00fe7c810ba88a2df
anisio-ifsp <anisio.silva@ifsp.edu.br>
2026-03-27 19:27:34 -0300

    aula 8

--- Aula_08_funcao_assincrona/src/exercicio.ts
+++ Aula_08_funcao_assincrona/src/exercicio.ts
@@ -1,4 +1,4 @@
-type Tarefa = { };
+type Tarefa = { descricao: string, prioridade: number, concluida: boolean};
 
 const tarefas: Tarefa[] = [
     { descricao: "Fazer relatório", prioridade: 2, concluida: false },
@@ -6,12 +6,29 @@ const tarefas: Tarefa[] = [
     { descricao: "Reunião com equipe", prioridade: 1, concluida: false }
 ];
 
-export function imprimirTarefa(descricao: string, indice: number, totalTarefas: number): void {
-    console.log(`Tarefa concluída: ${descricao}`);\
+function imprimirTarefa(descricao: string, indice: number, totalTarefas: number): void {
+    console.log(`Tarefa concluída: ${descricao}`);
     console.log(`Progresso: ${indice + 1}/${totalTarefas}`);
 }
 
-
-
-
+function executarTarefas(
+    tarefas: Tarefa[], 
+    callback:(descricao: string, indice: number, totalTarefas: number)=>void,
+    time:number= 1000
+):void{
+    let copiaTarefas = [...tarefas].sort((a,b)=> a.prioridade - b.prioridade);
+    let contador = 0;
+    const interval = setInterval(()=>{
+        if(contador >= tarefas.length || tarefas[contador]?.descricao === "Cancelar"){
+            clearInterval(interval);
+        }else{
+            const tarefa = tarefas[contador];
+            if(tarefa){
+                tarefa.concluida = true;
+                callback(tarefa.descricao,contador,tarefas.length);
+                contador++;
+            }
+        }
+    }, time );
+}
 executarTarefas(tarefas, imprimirTarefa, 1500);
